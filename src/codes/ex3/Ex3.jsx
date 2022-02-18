6f17ab431e59697207bcdb5311cb8a5bb0e0d991
unknown <ahmedomar1007@gmail.com>
2022-02-18 04:59:03 +0200

    setCount

--- src/codes/ex3/Ex3.jsx
+++ src/codes/ex3/Ex3.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 
 const Ex3 = () => {
-  const [count, setCount] = useState([0])
+  const [count, setCount] = useState(0)
+  const add = () => setCount((prevCount) => prevCount + 1)
+  const sub = () => setCount((prevCount) => prevCount - 1)
   return (
-    <div>
+    <div className='container mx-auto'>
       <h2>Counter</h2>
       <h3>{count}</h3>
-      <button>-</button>
-      <button>+</button>
+      <button onClick={add}>+</button>
+      <button onClick={sub}>-</button>
+      <h2>
+        Note: If you ever need the old value of state to help you determine the
+        new value of state, You should pass a callback function to your state
+        setter function instead of using state directly. This callback function
+        will receive the old value of state as parameter, which you can then use
+        to determine your new value of state.
+      </h2>
+      <a href='https://reactjs.org/docs/hooks-state.html'>Hooks State</a>
     </div>
   )
 }
