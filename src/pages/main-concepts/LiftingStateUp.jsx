9d487a8206566fb2a3e84b4444df2e5f7ccd50c7
unknown <ahmedomar1007@gmail.com>
2022-03-02 21:23:25 +0200

    source of truth

--- src/pages/main-concepts/LiftingStateUp.jsx
+++ src/pages/main-concepts/LiftingStateUp.jsx
@@ -100,9 +100,41 @@ function LiftingStateUp() {
   }
 
   return (
-    <>
+    <div className='container mx-auto px-4 m-3'>
       <Calculator />
-    </>
+      <p className='text-2xl'>Lessons Learned</p>
+      <p>
+        - There should be a single “source of truth” for any data that changes
+        in a React application. Usually, the state is first added to the
+        component that needs it for rendering. Then, if other components also
+        need it, you can lift it up to their closest common ancestor. Instead of
+        trying to sync the state between different components, you should rely
+        on the top-down data flow.
+      </p>
+      <p>
+        - Lifting state involves writing more “boilerplate” code than two-way
+        binding approaches, but as a benefit, it takes less work to find and
+        isolate bugs. Since any state “lives” in some component and that
+        component alone can change it, the surface area for bugs is greatly
+        reduced. Additionally, you can implement any custom logic to reject or
+        transform user input.
+      </p>
+      <p>
+        - If something can be derived from either props or state, it probably
+        shouldn’t be in the state. For example, instead of storing both
+        celsiusValue and fahrenheitValue, we store just the last edited
+        temperature and its scale. The value of the other input can always be
+        calculated from them in the render() method. This lets us clear or apply
+        rounding to the other field without losing any precision in the user
+        input.
+      </p>
+      <p>
+        - When you see something wrong in the UI, you can use React Developer
+        Tools to inspect the props and move up the tree until you find the
+        component responsible for updating the state.
+      </p>
+      <p>- Try!! Trace the bugs to their source in console</p>
+    </div>
   )
 }
 
